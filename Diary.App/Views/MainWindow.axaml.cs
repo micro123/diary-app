4ae6a0ec893e0e18c1a669fdb32831a700859e00
Guoli Tang <tangguoli9607@live.com>
2025-11-08 16:27:09 +0800

    标题栏使用反色主题

--- Diary.App/Views/MainWindow.axaml.cs
+++ Diary.App/Views/MainWindow.axaml.cs
@@ -1,12 +1,24 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Styling;
+using Avalonia.VisualTree;
 using Ursa.Controls;
 
 namespace Diary.App.Views
 {
     public partial class MainWindow: UrsaWindow
     {
+        private ThemeVariantScope? _titleBarScope = null;
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StyledElement_OnActualThemeVariantChanged(object? sender, EventArgs e)
+        {
+            var s = _titleBarScope ??= this.FindDescendantOfType<ThemeVariantScope>();
+            s!.RequestedThemeVariant = ActualThemeVariant == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
         }
     }
 }
